bc62db6d86b235fdafea56a3d00646ecc4baa2aa
Rbcerezer <rb.cerezer@gmail.com>
2023-10-06 00:28:07 +0200

    notes by types and variables

--- edu/rbcerezer/typesvariables/src/TypesVariables.java
+++ edu/rbcerezer/typesvariables/src/TypesVariables.java
@@ -1,5 +1,22 @@
 public class TypesVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double basicSalary = 2500;
+        //não representado por 2.500 - este seria decimais.
+
+        short smallNumber = 1;
+        int normalNUmber = smallNumber;
+        short smallNumber2 = (short) normalNUmber;
+
+        int number = 5;
+        number = 10;
+        System.out.println(number);
+
+        final VALOR_DE_PI = 3.14;
+        //para constantes preceder com "final" e nome em CAIXA ALTA
+
+        String name = "Rafael Cerezer";
+        //modo de tipo de texto
+
     }
 }
