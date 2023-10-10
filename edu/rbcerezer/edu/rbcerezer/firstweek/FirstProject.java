4838805bd84992e21b89c43c8d871134b0e1d860
Rbcerezer <rb.cerezer@gmail.com>
2023-10-11 01:08:50 +0200

    Cleanning files

--- edu/rbcerezer/edu/rbcerezer/firstweek/FirstProject.java
+++
@@ -1,8 +0,0 @@
-package edu.rbcerezer.firstweek;
-
-public class FirstProject {
-    public static void main(String[] args) {
-        System.out.println ("Hey there! Welcome to the journey...");
-    }
-    
-}
