de662ccf2db62493b4c02648844463c8d2c9ed5e
Rbcerezer <rb.cerezer@gmail.com>
2023-10-11 01:00:38 +0200

    New actualization

--- edu/rbcerezer/edu/rbcerezer/terminalArgs/AboutMe.java
+++ edu/rbcerezer/edu/rbcerezer/terminalArgs/AboutMe.java
@@ -6,25 +6,25 @@ import java.util.Scanner;
 public class AboutMe {
 
     public static void main(String[] args) {
-        //criando objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try (//criando objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+            System.out.println("Digite o seu nome: ");
+            String nome = scanner.next();
 
-        System.out.println("Digite o seu nome: ");
-        String nome = scanner.next();
+            System.out.println("Digite o seu sobrenome: ");
+            String sobrenome = scanner.next();
 
-        System.out.println("Digite o seu sobrenome: ");
-        String sobrenome = scanner.next();
+            System.out.println("Digite a sua idade: ");
+            int idade = scanner.nextInt();
 
-        System.out.println("Digite a sua idade: ");
-        int idade = scanner.nextInt();
+            System.out.println("Digite sua altura: ");
+            double altura = scanner.nextDouble();
 
-        System.out.println("Digite sua altura: ");
-        double altura = scanner.nextDouble();
-
-        //imprimindo os dados coletados
-        System.out.println("Olá pessoal! Eu sou: " + nome + " " + sobrenome);
-        System.out.println("Tenho apenas " + idade + " anos ");
-        System.out.println("E minha altura é: " + altura);
+            //imprimindo os dados coletados
+            System.out.println("Olá pessoal! Eu sou: " + nome + " " + sobrenome);
+            System.out.println("Tenho apenas " + idade + " anos ");
+            System.out.println("E minha altura é: " + altura);
+        }
     }
     
 }
